b68851b9694fab0c0a1b774f4279ef4bbb888d89
Josias Santos <josiassantos1577@gmail.com>
2022-12-03 00:15:32 -0300

    Links social icons

--- personal-portfolio-next/src/components/Footer.jsx
+++ personal-portfolio-next/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
   return (
@@ -6,15 +7,18 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src="./img/logo.svg" alt="Logo" />
+          <div className="social-icon mt-4">
+            <img src="https://github.com/josiassantos15.png" class="border border-white rounded-circle" alt="Logo"/>
+            <p>Team name</p>
+          </div>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src='./img/nav-icon1.svg' alt="Icon" /></a>
-              <a href="#"><img src='./img/nav-icon2.svg' alt="Icon" /></a>
-              <a href="#"><img src='./img/nav-icon2.svg' alt="Icon" /></a>
+            <div className="social-icon mt-4">
+              <a href="https://www.linkedin.com/in/josias-santos-265319179/" target="_blank" rel="noopener noreferrer"><img src='./img/nav-icon1.svg' alt="Icon" /></a>
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><img src='./img/nav-icon2.svg' alt="Icon" /></a>
+              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src='./img/nav-icon3.svg' alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright {currentYear}. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
